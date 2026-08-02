58a23dd338c4aef94ca55a132932af4b7baa6ed5
Tushar Kaldate <tusharkaldate110@gmail.com>
2026-08-03 01:20:15 +0530

    Refactor leaders function and add alternative solution
    Refactor leaders function to improve efficiency and clarity. Added a second solution with sorting approach.

--- 07_Arrays/023_Leaders_in_an_Array.cpp
+++ 07_Arrays/023_Leaders_in_an_Array.cpp
@@ -1,12 +1,39 @@
+
 class Solution {
 public:
     vector<int> leaders(vector<int>& nums) {
         int n  = nums.size(); 
-        vector<int> ans;   
+        int max = nums[n-1];
+        vector<int> ans;
+        ans.push_back(max);
+
+        for(int i = n -2 ; i >= 0 ; i--){
+            if(nums[i] > max){
+                ans.push_back(nums[i]);
+                max = nums[i];
+            }
+        }
+
+        reverse(ans.begin() , ans.end());      
+
+        return ans;
+    }
+};
+
+
+
+class Solution1 {
+public:
+    vector<int> leaders(vector<int>& nums) {
+        int n  = nums.size(); 
+        vector<int> ans;  
+        vector<int> temp = nums; 
         for(int i = 0  ; i  <n ; i++){
-            if(nums[i] > max(nums[i+1] ,nums[n-1])){
+            sort(temp.begin()+i+1 , temp.end());
+            if(nums[i] > temp[n-1]){
                 ans.push_back(nums[i]);
             }
+            temp = nums;
         }
         ans.push_back(nums[n-1]);
         return ans;
